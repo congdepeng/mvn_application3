1b428e6bb845a11fc8588d5d55a3288a8056d022
Cong Depeng <congdepeng@gmail.com>
2014-01-02 00:43:27 +0800

    finish

---
+++ appC/src/main/java/com/depeng/main/AppMain.java
@@ -0,0 +1,16 @@
+package com.depeng.main;
+
+import com.depeng.liba.SayHello;
+
+/**
+ * Created by depeng on 1/1/14.
+ */
+public class AppMain {
+
+    public static void main(String[] args) {
+
+        SayHello sayHello = new SayHello();
+        sayHello.say();
+
+    }
+}
---
+++ libA/src/main/java/com/depeng/liba/SayHello.java
@@ -0,0 +1,10 @@
+package com.depeng.liba;
+
+/**
+ * Created by depeng on 1/1/14.
+ */
+public class SayHello {
+    public void say() {
+        System.out.println("Hello Maven!");
+    }
+}
